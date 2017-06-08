90873252468197207aeafd43a0154b1bab90deb4
synical <NA>
2017-06-08 22:01:34 +0100

    Refactor main and sort by diff

--- intop.py
+++ intop.py
@@ -8,12 +8,42 @@ from time import sleep
 
 #TODO
 #
-# Order by greatest delta and not sum
-# Make get_top_interrupts function
+# Add arg for printing by overall sum
+# Add arg for printing by diff
+# Add arg for setting sleep interval
+
+def diff_interrupt_sums(before, after):
+    interrupt_dict_diff = defaultdict(dict)
+    for k, v in before.iteritems():
+        diff = max(v["sum"], after[k]["sum"]) - min(v["sum"], after[k]["sum"])
+        interrupt_dict_diff[k]["name"] = v["name"]
+        interrupt_dict_diff[k]["sum"] = diff
+    print_top_interrupts(interrupt_dict_diff)
 
 def get_cpus(cpu_line):
     cpu_regex = r"CPU[0-9]+"
     return re.findall(cpu_regex, cpu_line)
+
+def parse_interrupts():
+    interrupt_dict = defaultdict(dict)
+    with open("/proc/interrupts") as f:
+        for i, line in enumerate(f.readlines()):
+            if i == 0:
+                cpus = get_cpus(line)
+                num_cpus = len(cpus)
+                continue
+            split_line = filter(None, line.strip("\n").replace(":", "").split(" "))
+            device_name = " ".join(split_line[num_cpus+1:])
+            cpu_sum = sum_interrupts(num_cpus, split_line)
+            interrupt_dict[split_line[0]]["name"] = device_name
+            interrupt_dict[split_line[0]]["sum"] = cpu_sum
+        f.close()
+    return interrupt_dict
+
+def print_top_interrupts(interrupt_dict):
+        top_interrupts = sorted(interrupt_dict, key=lambda i: interrupt_dict[i]["sum"], reverse=True)
+        for i in top_interrupts:
+            print "%s (%s) -> %s" % (i, interrupt_dict[i]["name"], interrupt_dict[i]["sum"])
 
 def sum_interrupts(num_cpus, split_line):
     cpu_sum = 0
@@ -27,25 +57,12 @@ def sum_interrupts(num_cpus, split_line):
 
 def main():
     system("clear")
-    interrupt_dict = defaultdict(dict)
     while True:
-        with open("/proc/interrupts") as f:
-                for i, line in enumerate(f.readlines()):
-                    if i == 0:
-                        cpus = get_cpus(line)
-                        num_cpus = len(cpus)
-                        continue
-                    split_line = filter(None, line.strip("\n").replace(":", "").split(" "))
-                    device_name = " ".join(split_line[num_cpus+1:])
-                    cpu_sum = sum_interrupts(num_cpus, split_line)
-                    interrupt_dict[split_line[0]]["name"] = device_name
-                    interrupt_dict[split_line[0]]["sum"] = cpu_sum
-                top_interrupts = sorted(interrupt_dict, key=lambda i: interrupt_dict[i]["sum"], reverse=True)
-                for i in top_interrupts:
-                    print "%s (%s) -> %s" % (i, interrupt_dict[i]["name"], interrupt_dict[i]["sum"])
-                f.close()
-                sleep(2)
-                system("clear")
+        interrupt_dict_before = parse_interrupts()
+        sleep(2)
+        system("clear")
+        interrupt_dict_after = parse_interrupts()
+        diff_interrupt_sums(interrupt_dict_before, interrupt_dict_after)
 
 if __name__ == '__main__':
     main()
