2c265b8e5e5e30fcf4c70847557841c929e21cef
synical <NA>
2017-06-08 19:27:00 +0100

    Loop and refresh every 2 seconds

--- intop.py
+++ intop.py
@@ -3,6 +3,8 @@
 import re
 
 from collections import defaultdict
+from os import system
+from time import sleep
 
 def get_cpus(cpu_line):
     cpu_regex = r"CPU[0-9]+"
@@ -21,17 +23,20 @@ def sum_interrupts(num_cpus, split_line):
 def main():
     interrupt_dict = {}
     with open("/proc/interrupts") as f:
-        for i, line in enumerate(f.readlines()):
-            if i == 0:
-                cpus = get_cpus(line)
-                num_cpus = len(cpus)
-                continue
-            split_line = filter(None, line.strip("\n").replace(":", "").split(" "))
-            cpu_sum = sum_interrupts(num_cpus, split_line)
-            interrupt_dict[split_line[0]] = cpu_sum
-        top_interrupts = sorted(interrupt_dict, key=interrupt_dict.get, reverse=True)
-        for i in top_interrupts:
-            print "[%s] -> %s" % ()
+        while True:
+            for i, line in enumerate(f.readlines()):
+                if i == 0:
+                    cpus = get_cpus(line)
+                    num_cpus = len(cpus)
+                    continue
+                split_line = filter(None, line.strip("\n").replace(":", "").split(" "))
+                cpu_sum = sum_interrupts(num_cpus, split_line)
+                interrupt_dict[split_line[0]] = cpu_sum
+            top_interrupts = sorted(interrupt_dict, key=interrupt_dict.get, reverse=True)
+            for i in top_interrupts:
+                print "[%s] -> %s" % (i, interrupt_dict[i])
+            sleep(2)
+            system("clear")
 
 if __name__ == '__main__':
     main()
