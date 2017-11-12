e438255ee24f3198895e913d790ff807b29fd7d3
synical <no@no.no>
2017-11-12 22:38:49 +0000

    Add tsastat tool outline

---
+++ tsastat.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+Print time spent in each of the six thread states
+  - Start system wide
+  - Since boot with no interval
+  - Diff per interval with interval arg passed
+*/
+
+func main() {
+	return
+}
