e1a270a592a0056b52538e157f69a1c9aa6b058e
synical <no@no.no>
2017-11-23 04:31:43 +0000

    Get all pids and their current state

--- tsastat.go
+++ tsastat.go
@@ -1,12 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
 /*
 Print time spent in each of the six thread states
-  - Start system wide
-  - Since boot with no interval
+  - Start for all threads of a process
   - Diff per interval with interval arg passed
+
+I think the point of this in the Brendan Gregg book
+is to demonstrate that this is quite hard in Linux
 */
 
+func getThreadState(pid string) string {
+	status, err := ioutil.ReadFile("/proc/" + pid + "/status")
+	if err != nil {
+		log.Fatal("Failed to read status file: ", err)
+	}
+	stateLine := strings.Split(string(status), "\n")[2]
+	state := string(strings.Split(stateLine, ":")[1][1])
+	return state
+}
+
 func main() {
+	tsMap := make(map[int]string)
+	dirs, err := ioutil.ReadDir("/proc/")
+	if err != nil {
+		log.Fatal("Failed to read /proc: ", err)
+	}
+
+	for _, dir := range dirs {
+		name := dir.Name()
+		if pid, err := strconv.Atoi(name); err == nil {
+			tsMap[pid] = getThreadState(name)
+		}
+	}
+
+	for pid, state := range tsMap {
+		fmt.Printf("%d: %s\n", pid, state)
+	}
+
 	return
 }
